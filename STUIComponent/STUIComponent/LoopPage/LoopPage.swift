44453dd126f07704feee973e733d1cb3fd9b6e2a
Azen <azen.cell@foxmail.com>
2016-02-25 16:07:36 +0800

    LoopPage推到重来

--- STUIComponent/STUIComponent/LoopPage/LoopPage.swift
+++ STUIComponent/STUIComponent/LoopPage/LoopPage.swift
@@ -6,29 +6,6 @@
 //  Copyright © 2016年 st company. All rights reserved.
 //
 
-public protocol LoopPageDelegate : UITableViewDelegate {
-    func loopPage(numberOfPagesInLoopPage: LoopPage) -> Int
-    func loopPage(loopPage: LoopPage , pageViewAtIndex: Int) -> UIView
-    func loopPage(loopPage: LoopPage , didSelectPageAtIndex: Int)
-}
-
 public class LoopPage: UIView {
-    
-    public weak var delegate : LoopPageDelegate?
-    var collection : UICollectionView?
-    var pageControl : UIPageControl?
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override public func layoutSubviews() {
-        super.layoutSubviews()
-        
-    }
     
 }
