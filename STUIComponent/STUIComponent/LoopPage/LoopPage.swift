7984777b947c3ba5dc35dfcd6c2f3dbfbbc8204c
Azen <azen.cell@foxmail.com>
2016-02-25 17:01:18 +0800

    CollectionView实现

--- STUIComponent/STUIComponent/LoopPage/LoopPage.swift
+++ STUIComponent/STUIComponent/LoopPage/LoopPage.swift
@@ -7,5 +7,43 @@
 //
 
 public class LoopPage: UIView {
+    let collectionLayoyt : UICollectionViewFlowLayout?
+    let collectionView : UICollectionView?
     
+    override public init(frame: CGRect) {
+        
+        collectionLayoyt = UICollectionViewFlowLayout()
+        collectionLayoyt!.itemSize = CGSize(width: frame.width, height: frame.height)
+        
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: collectionLayoyt!)
+        collectionView!.backgroundColor = UIColor.purpleColor()
+        collectionView?.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "loopCell")
+        
+        super.init(frame: frame)
+        
+        collectionView!.dataSource = self
+        addSubview(collectionView!)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        collectionLayoyt?.itemSize = bounds.size
+        collectionView?.frame = bounds
+    }
+}
+
+extension LoopPage : UICollectionViewDataSource {
+    public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let page = collectionView.dequeueReusableCellWithReuseIdentifier("loopCell", forIndexPath: indexPath)
+        page.backgroundColor = UIColor.blackColor()
+        return page
+    }
 }
