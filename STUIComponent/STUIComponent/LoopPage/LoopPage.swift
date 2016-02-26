1b76eab8354f283e7f81a909fc6dde4459f6aecc
Azen <azen.cell@foxmail.com>
2016-02-26 10:14:38 +0800

    LoopPage 继续中

--- STUIComponent/STUIComponent/LoopPage/LoopPage.swift
+++ STUIComponent/STUIComponent/LoopPage/LoopPage.swift
@@ -7,22 +7,13 @@
 //
 
 public class LoopPage: UIView {
-    let collectionLayoyt : UICollectionViewFlowLayout?
-    let collectionView : UICollectionView?
+
+    var collectionView : UICollectionView?
+    
     
     override public init(frame: CGRect) {
-        
-        collectionLayoyt = UICollectionViewFlowLayout()
-        collectionLayoyt!.itemSize = CGSize(width: frame.width, height: frame.height)
-        
-        collectionView = UICollectionView(frame: frame, collectionViewLayout: collectionLayoyt!)
-        collectionView!.backgroundColor = UIColor.purpleColor()
-        collectionView?.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "loopCell")
-        
         super.init(frame: frame)
-        
-        collectionView!.dataSource = self
-        addSubview(collectionView!)
+    
     }
 
     required public init?(coder aDecoder: NSCoder) {
@@ -31,19 +22,59 @@ public class LoopPage: UIView {
     
     override public func didMoveToSuperview() {
         super.didMoveToSuperview()
-        collectionLayoyt?.itemSize = bounds.size
-        collectionView?.frame = bounds
+        setupBasic()
     }
+    
+    private func setupBasic() {
+        // MARK: - 初始化collection
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: frame.width, height: frame.height)
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collectionView?.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "loopCell")
+        collectionView!.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.showsVerticalScrollIndicator = false
+        collectionView?.showsHorizontalScrollIndicator = false
+        collectionView?.pagingEnabled = true
+        addSubview(collectionView!)
+        
+        pageCtl.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
+        pageCtl.frame.origin.x = (frame.width - pageCtl.frame.width) * 0.5
+        pageCtl.frame.origin.y = frame.height - pageCtl.frame.height
+        addSubview(pageCtl)
+        
+    }
+    
+    
+    //KARK: - Lazy
+    let pageCtl : UIPageControl = {
+        let pageCtl = UIPageControl()
+        pageCtl.numberOfPages = 5
+        pageCtl.backgroundColor = UIColor.redColor()
+        return pageCtl
+    }()
 }
 
-extension LoopPage : UICollectionViewDataSource {
+extension LoopPage : UICollectionViewDataSource, UICollectionViewDelegate {
     public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let page = collectionView.dequeueReusableCellWithReuseIdentifier("loopCell", forIndexPath: indexPath)
-        page.backgroundColor = UIColor.blackColor()
+        
+        page.backgroundColor = UIColor(red: CGFloat(Double(arc4random_uniform(100)) * 0.01), green: CGFloat(Double(arc4random_uniform(100)) * 0.01), blue: CGFloat(Double(arc4random_uniform(100)) * 0.01), alpha: 0.5)
         return page
     }
+    
+    public func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let currentPageIndex = collectionView.indexPathsForVisibleItems().first
+        print(currentPageIndex)
+        pageCtl.currentPage = (currentPageIndex?.item)!
+    }
+    
 }
